131464cbdf62ce230feccb757a908ee33a90cc9d
MichalNowicki7 <now.mic7@wp.pl>
2021-05-24 14:30:03 +0200

    First commit

---
+++ src/main/java/org/masteukodeu/swinka/SwinkaController.java
@@ -0,0 +1,39 @@
+package org.masteukodeu.swinka;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SwinkaController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+/*import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SwinkaController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello World!";
+    }
+}*/
